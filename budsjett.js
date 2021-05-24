61f1a8b8061b2fb0ff99f41abe5ce62c9fefe312
HekenBleken <haakonmyhren@gmail.com>
2021-05-24 18:04:19 +0200

    Update budsjett.js
    Håkon små endringer JS

--- budsjett.js
+++ budsjett.js
@@ -11,7 +11,7 @@ let utgMengde = document.getElementById("utgMengde");
 let id = 0;
 let details = [];
 
-console.log(utgForm);
+
 
 
 function getBudsjettMengde(mengde) {
@@ -26,7 +26,7 @@ function getBudsjettMengde(mengde) {
     } else {
         budsjettMengde.innerText = mengde;
         balanseMengde.innerText = mengde;
-        utgiftMottaker.style.display = "block";
+        utgNavn.style.display = "block";
         budsjettMottaker.style.display = "none";
         endreMottaker.style.display = "none";
         mengdeInput.value = "";
@@ -37,7 +37,6 @@ lagForm.addEventListener("submit", (e) => {
     e.preventDefault();
     getBudsjettMengde(mengdeInput.value);
 });
-
 
 function addUtgifter(utgNavn, utgMengde) {
     if (!utgNavn.length || !utgMengde.length) {
@@ -72,3 +71,37 @@ utgForm.addEventListener("submit", (e) => {
     e.preventDefault();
     addUtgifter(utgNavn.value, utgMengde.value);
 });
+
+function visUtg(details) {
+    utgMengde.innerHTML = null;
+    for (i = 0; i < details.length; i++) {
+        utgMengde.innerHTML += `
+        <div class="utgMengde" id="kr.{details[i].id}">
+            <div id= "utgNavn" class="utg"<p>kr.{detailst[i].name}</p></div>
+            <div id= "utgMengde" class "utg"><p> <span>kr. </span>kr.{details[i].number}</p></div>
+            <div id= "endreOgSlette">
+                <p>
+                    <button id="kr.{details[i].id}" onclick="endreUtg(kr.{details[i].id})">
+                    <button id="kr.{details[i].id}" onclick="slettUtg(kr.{details[i].id})">
+                </p>
+            </div>
+        </div>
+        `;
+    }
+    regnUtg();
+    visUtg.style.display = "block";
+}
+
+function regnUtg() {
+    let totalUtg = 0;
+    for (i = 0; i < details.length; i++) {
+        totalUtg = details[i].number + totalUtg;
+    }
+    utgMengde.innerText = totalUtg;
+    nyBalanse();
+}
+
+function nyBalanse() {
+    balanseMengde.innerText =
+    parseInt(budsjettMengde.innerText) - parseInt(utgMengde.innerText);
+}
